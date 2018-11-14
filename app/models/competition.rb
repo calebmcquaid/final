6552895999c9c0f9a985aa982a35b8f243606952
Caleb McQuaid <calebmcquaid@gmail.com>
2018-11-14 17:29:37 -0500

    moar functunality

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,20 +4,54 @@ class Competition < ApplicationRecord
   has_many :stats
   
   def fetch_fitbit_data
-    client = FitbitAPI::Client.new(
-      client_id: ENV['FITBIT_KEY'],
-      client_secret: ENV['FITBIT_SECRET'],
-      access_token: users.first.fitbit_access_token,
-      refresh_token: users.first.fitbit_refresh_token,
-      expires_at: 31536000,
-      user_id: users.first.fitbit_user_id
-    )
-    
-    steps = client.activity_time_series("steps", start_date: start_date, end_date: end_date)
-    calories = client.activity_time_series("calories", start_date: start_date, end_date: end_date)
-    miles = client.activity_time_series("distance", start_date: start_date, end_date: end_date)
+    users.each do |user|
+      client = FitbitAPI::Client.new(
+        client_id: ENV['FITBIT_KEY'],
+        client_secret: ENV['FITBIT_SECRET'],
+        access_token: user.fitbit_access_token,
+        refresh_token: user.fitbit_refresh_token,
+        expires_at: 31536000,
+        user_id: user.fitbit_user_id
+      )
 
-    prepped_stats = steps
+      steps = client.activity_time_series("steps", start_date: start_date, end_date: end_date)
+      calories = client.activity_time_series("calories", start_date: start_date, end_date: end_date)
+      miles = client.activity_time_series("distance", start_date: start_date, end_date: end_date)
+
+      prepped_stats = steps
+        .pluck("value")
+        .zip(calories.pluck("value"), miles.pluck("value"), steps.pluck("dateTime"))
+        .map do |stats| 
+          {steps: stats[0], calories: stats[1], miles: stats[2], recorded_at: stats[3]}
+        end
+
+      prepped_stats.each do |stat_data|
+        stat = Stat.new(stat_data)
+        stat.user = users.first
+        stat.competition = self
+        stat.skip_fetch = true
+        stat.save!
+      end
+    end
+  end
+
+  def fetch_strava_data
+    unless @skip_fetch
+      puts "SOMETHING IS GOING WRONG"
+      client = Strava::Api::V3::Client.new(
+        client_id: ENV['STRAVA_KEY'],
+        client_secret: ENV['STRAVA_SECRET'],
+        token_type: 'Bearer',
+        access_token: user.strava_access_token,
+        refresh_token: user.strava_refresh_token,
+        expires_at: 3153600,
+        )
+
+      miles = client.list_athlete_activities("distance", after: start_date, before: end_date)
+      calories = (client.list_athlete_activities("distance", after: start_date, before: end_date)) * 100
+      steps = ((client.list_athlete_activities("distance", after: start_date, before: end_date)) * 5280) / 2.3
+      
+      prepped_stats = steps
       .pluck("value")
       .zip(calories.pluck("value"), miles.pluck("value"), steps.pluck("dateTime"))
       .map do |stats| 
@@ -31,8 +65,7 @@ class Competition < ApplicationRecord
       stat.skip_fetch = true
       stat.save!
     end
-  end
-
-  def fetch_strava_data
+    end
   end
 end
+
